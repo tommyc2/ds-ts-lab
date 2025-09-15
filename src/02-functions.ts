a8e04836b69884c8a726acadba56ba736b2a1bcb
tommyc2 <20101841@mail.wit.ie>
2025-09-15 16:48:31 +0100

    Function to update friend's interests.
    Signed-off-by: tommyc2 <20101841@mail.wit.ie>

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -54,9 +54,9 @@ function sortColleagues(colleagues: Colleague[],sorter: (c1: Colleague, c2: Coll
 }
 
 // Test invocations
-console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+//console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
 
   
   //console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
@@ -69,3 +69,13 @@ console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name
 //console.log(findFriends(friends, (friend) => friend.age < 35));
 //console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 
+function addInterestToFriend(friend: Friend, interest: string) {
+  if (!friend.interests) {
+    friend.interests = []
+  }
+  friend.interests?.push(interest)
+  return friend.interests
+}
+
+console.log(addInterestToFriend(friends[0], 'Programming'));
+console.log(addInterestToFriend(friends[0], 'Testing'));
